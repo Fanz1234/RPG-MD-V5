0d32415f0e43bebda35d5c2b4594c0f213a32948
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2022-12-26 19:57:37 +0700

    Update downloader-ttaudio.js

--- plugins/downloader-ttaudio.js
+++ plugins/downloader-ttaudio.js
@@ -55,8 +55,8 @@ const time = moment.tz('Asia/Jakarta').format('HH')
                         description: 'anu',
                         title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
                         body: `Subscribe YT My Bestie`,                                       previewType: 0,
-                        thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
-                        sourceUrl: 'https://youtu.be/pwLZpdfO8AU'
+                        thumbnail: await (await fetch(`https://telegra.ph/file/e83ba3c272120b6b3ed05.jpg`)).buffer(),
+                        sourceUrl: 'https://youtu.be/'
             }
         }
     })
